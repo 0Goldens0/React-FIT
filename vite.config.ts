025bcc75b2af8671b767d2d6c12951b9e93d3df7
Алексей Кожевников <EcoKozhevnikov@yandex.ru>
2025-10-22 15:04:48 +0300

    адаптив брендов и таймлайна

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config: any = {
     plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [
+          autoprefixer({
+            overrideBrowserslist: [
+              '>0.2%',
+              'not dead',
+              'not op_mini all',
+              'iOS >= 10',
+              'Safari >= 10',
+              'Chrome >= 60',
+              'Firefox >= 60',
+              'Edge >= 79',
+              'Samsung >= 8'
+            ],
+            grid: 'autoplace',
+            flexbox: 'no-2009'
+          })
+        ]
+      }
+    },
     server: {
       port: 3000,
       host: true
